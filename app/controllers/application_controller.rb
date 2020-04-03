303dea3ef48ead101a6d0079be1416590c348e86
足立守 <rooreckless@gmail.com>
2020-04-04 05:02:03 +0900

    コントローラ作成 トップページの表示用ルートを設定
    また、ルートはcards#topですが、このアクションは実行前にログインが必要にしています。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # 処理がdeviseに関するコントローラが起動したなら、sanitizerでnicknameをpermitします。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
 end
