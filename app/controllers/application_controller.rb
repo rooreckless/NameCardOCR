36fe70d5ee14181aa4b933421f058b0257bd432a
足立守 <rooreckless@gmail.com>
2020-04-16 12:53:54 +0900

    Basic認証導入
    本番環境のみBasic認証が入ります

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   # 処理がdeviseに関するコントローラが起動したなら、sanitizerでnicknameをpermitします。
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+
   protected
   def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:group_id,:owned_group_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:group_id,:owned_group_id])
   end
   # def after_sign_in_path_for(resource)
   #   mypage_root_path # ログイン後に遷移するpathを設定
@@ -13,4 +15,14 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     new_user_session_path # ログアウト後に遷移するpathを設定
   end
+  private
+    def production?
+      Rails.env.production?
+    end
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == Rails.application.credentials[:BasicAuth][:BasicAuthUser] && password == Rails.application.credentials[:BasicAuth][:BasicAuthPassword].to_s
+        # passwordの方は、強制的に文字列にしています。(なんでかは書きませんが。)
+      end
+    end
 end
