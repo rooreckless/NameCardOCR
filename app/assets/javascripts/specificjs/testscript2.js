3126a88c416c74a236460d29521453de2a73c066
足立守 <rooreckless@gmail.com>
2020-04-18 15:43:27 +0900

    本番環境用のjs設定
    本番環境でのjsをプリコンパイルするよう設定し、test用のjsを削除

--- app/assets/javascripts/specificjs/testscript2.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  console.log('===this message is testscript2.js===');
-  var reloadTestConsole = function(){
-    console.log('--reloadTestConsole22222222--');
-  }
-  setInterval(reloadTestConsole, 7000);
-});
